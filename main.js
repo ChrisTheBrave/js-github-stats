89530223700feacc9b240331e8b294af06e5692a
Christine Ash <ChristineTheBrave@Christines-MacBook-Pro.local>
2017-03-07 08:56:47 -0500

    Tried to fix issue with open curly brace

--- main.js
+++ main.js
@@ -38,11 +38,55 @@ let mostStars = fetch(
   }
 );
 
+
   mostStars.then( function countStars(responseObj) {
     responseObj.json().then(function printData(data) {
       console.log(data[0].stargazers_count);
+      let mostStars = 0;
+      let repoName;
+      let ownerName;
+
       data.forEach(function compareStars(each) {
         console.log(each.stargazers_count);
+        if (each.stargazers_count > mostStars) {
+          mostStars = each.stargazers_count;
+          repoName = each.name;
+          ownerName = each.owner.login;
+        }
+
+
       });
-    });
-  });
+      console.log(mostStars, repoName, ownerName);
+
+      // here
+      let secondMostStars = fetch(
+        'https://api.github.com/repos/'+ownerName+'/'+repoName+'/contributors',
+        {
+          method: 'GET',
+          headers: {
+            Authorization:'token '+process.argv[3]
+
+          }
+        }
+      );
+
+      mostStars.then( function countStars(responseObj) {
+        responseObj.json().then(function printData(data) {
+          console.log(data[0].stargazers_count);
+          if ( responseObj.status > 199 && responseObj.status < 300) {
+            responseObj.json().then( function compareStars(contributors) {
+              contributors.forEach(function anotherContributor(eachContributor) {
+                console.log(eachContributor.login);
+              });
+            });
+          } else {
+            console.log('We gotta problem fam');
+          }
+        });
+      });
+
+    } else {
+      console.log('We gotta problem fam ');
+    }
+
+);
